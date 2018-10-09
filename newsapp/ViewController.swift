0a690ebba751686587bc277c47dd99a75099c359
ebwinters@comcast.net <ebwinters@comcast.net>
2018-10-09 12:40:25 -0400

    Initial Commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by Ethan Winters on 10/9/18.
+//  Copyright © 2018 CMSC424IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
