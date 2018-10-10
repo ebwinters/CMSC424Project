74461ee101a0f535ea836e046c392137f8452400
ebwinters@comcast.net <ebwinters@comcast.net>
2018-10-10 16:41:22 -0400

    remove user works

--- newsapp/userAuthenticatedViewController.swift
+++ newsapp/userAuthenticatedViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class userAuthenticatedViewController: UIViewController {
     var userID = ""
-
+    var ref:DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        ref = Database.database().reference()
         print (self.userID)
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func deleteCurrentUser(_ sender: Any) {
+        if let users = Auth.auth().currentUser {
+            users.delete { (error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                try! Auth.auth().signOut()
+                print("User deleted")
+                self.performSegue(withIdentifier: "deletedUser", sender: self)
+                
+            }
+        }
+    }
+    
     
 
     /*
