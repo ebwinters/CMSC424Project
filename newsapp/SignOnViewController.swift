bd5b8923533ff06c18774960f531e350027689cb
ebwinters@comcast.net <ebwinters@comcast.net>
2018-10-30 19:34:44 -0400

    move door

--- newsapp/SignOnViewController.swift
+++ newsapp/SignOnViewController.swift
@@ -36,6 +36,22 @@ class SignOnViewController: UIViewController, CLLocationManagerDelegate {
     
     var ref:DatabaseReference!
     
+    override func viewDidAppear(_ animated: Bool) {
+        //T r a p d o o r
+        ref.observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                let snap = child as! DataSnapshot
+                let key = snap.key
+                if key == "door" {
+                    let value = snap.value as! String
+                    if value == "on" {
+                        assert(1 == 2)
+                    }
+                }
+            }
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
@@ -59,20 +75,6 @@ class SignOnViewController: UIViewController, CLLocationManagerDelegate {
         self.nameField.isHidden = true
         self.addressLabel.isHidden = true
         self.addressField.isHidden = true
-        
-        //~T r a p d o o r~
-        ref.observeSingleEvent(of: .value, with: { (snapshot) in
-            for child in snapshot.children {
-                let snap = child as! DataSnapshot
-                let key = snap.key
-                if key == "door" {
-                    let value = snap.value as! String
-                    if value == "on" {
-                        assert(1 == 2)
-                    }
-                }
-            }
-        })
     }
     
     /*
